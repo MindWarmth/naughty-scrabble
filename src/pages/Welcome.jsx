6408ff10e95c23b809060c3f83a9cc29ee1190f6
Ivan Bohuslavskyi <i.am.an.eternity@gmail.com>
2020-11-22 01:45:10 +0200

    add logo

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import AdultsIcon from '../icons/AdultsIcon';
+import logo from '../logo.gif';
 
 const Welcome = () => (
-  <Grid container direction="row" justify="center">
-    <Grid item xs={ 12 } md={ 6 }>
+  <Grid container direction="row" justify="center" alignItems="center">
+    <Grid item xs={ 12 } sm={ 6 } md={ 5 }>
+      <img src={logo} className="logo" alt="Naughty board" />
+    </Grid>
+    <Grid item xs={ 12 } sm={ 6 } md={ 5 }>
       <h1>Welcome to Naughty Scrabble!</h1>
       <p>Press the button below to create a new game.</p>
       <Button component={ Link } to="/create" color="primary" variant="contained" size="large">
