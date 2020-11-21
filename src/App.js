cac4d090fc41814bf0302605688562f7d41ee8b0
Roman Vabishchevych <r@vabishchevych.com>
2020-11-21 02:41:35 +0200

    theme

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import Welcome from './pages/Welcome';
 import Create from './pages/Create';
 import Invite from './pages/Invite';
@@ -12,33 +12,46 @@ import Game from './pages/Game';
 import './App.css';
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+      primary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#fff',
+      },
+    }
+  });
   return (
     <Router>
-      <div className="app">
-        <Switch>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <Switch>
 
-          <Route path="/create">
-            <Create />
-          </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
 
-          <Route path="/invite">
-            <Invite />
-          </Route>
+            <Route path="/invite">
+              <Invite />
+            </Route>
 
-          <Route path="/join/:gameID">
-            <Join />
-          </Route>
+            <Route path="/join/:gameID">
+              <Join />
+            </Route>
 
-          <Route path="/game/:gameID">
-            <Game />
-          </Route>
-         
-          <Route path="/">
-            <Welcome />
-          </Route>
+            <Route path="/game/:gameID">
+              <Game />
+            </Route>
+          
+            <Route path="/">
+              <Welcome />
+            </Route>
 
-        </Switch>        
-      </div>
+          </Switch>        
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
