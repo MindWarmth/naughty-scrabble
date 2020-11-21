64b67ffaff7f3b325beacb0585d9a614fdaa6b51
Ivan Bohuslavskyi <i.am.an.eternity@gmail.com>
2020-11-21 21:11:34 +0200

    add leave button

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,15 +1,30 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 import { makeStyles } from '@material-ui/core/styles';
+import Context from '../../context';
 import './Controls.css';
 
 const useStyles = makeStyles((theme) => ({
   btn: {
-    margin: '0 2px',
+    fontSize: '11px',
+    margin: '0 1px',
   },
 }));
 
 function Controls() {
   const classes = useStyles();
+  const history = useHistory();
+  const { setGameID, setDictionary, setUser } = useContext(Context);
+
+  const handleLeaveClick = () => {
+    setGameID(null);
+    setDictionary([]);
+    setUser(null);
+    history.push('/');
+  };
 
   return (
     <div className="controls">
@@ -25,6 +40,9 @@ function Controls() {
       <Button variant="contained" size="small" className={classes.btn}>
         Swap
       </Button>
+      <IconButton aria-label="Leave" component="span" size="small" onClick={handleLeaveClick}>
+        <MeetingRoomIcon />
+      </IconButton>
     </div>
   );
 }
