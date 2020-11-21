f144cb4ce21aff3ec8c108a88dde794d6f0afc6d
Roman Vabishchevych <r@vabishchevych.com>
2020-11-21 20:28:59 +0200

    enter Letter to board

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,6 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Redirect, useParams } from 'react-router-dom';
+import set from 'lodash/fp/set';
 import Board from '../components/Board';
 import Log from '../components/Log';
 import Scoreboard from '../components/Scoreboard';
@@ -8,10 +9,17 @@ import Context from '../context';
 
 const Game = () => {
   const params = useParams();
-  const { gameID, dictionary } = useContext(Context);
+  const { gameID = 'test', dictionary } = useContext(Context);
+  const [ fieldsData, setFieldsData ] = useState({});
+  const [ canPlay, setCanPlay ] = useState(true)
 
   if (!gameID && params.gameID) {
     return <Redirect to={`/join/${params.gameID}`} />
+  }
+
+  const handleOnBoardChange = ({ row, col, letter }) => {
+    setCanPlay(false);
+    setFieldsData(set(`${row}.${col}`, letter, fieldsData));
   }
 
   return (
@@ -19,7 +27,7 @@ const Game = () => {
       <h1 className="title">Game ID: {gameID}</h1>
       <div className="game">
         <div className="main">
-          <Board />
+          <Board fieldsData={fieldsData} onChange={ handleOnBoardChange } canPlay={canPlay}/>
         </div>
         <div className="sidebar">
           <Log />
