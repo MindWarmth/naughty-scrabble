207ed584a0967f9b75dd883452e2fc765bbc43e8
Ivan Bohuslavskyi <i.am.an.eternity@gmail.com>
2020-11-22 06:23:42 +0200

    Updates

--- workers/dictionary.js
+++ workers/dictionary.js
@@ -1,11 +1,16 @@
 onmessage = function({ data }) {
-  console.info('Worker: Message received from main script', data);
+  console.info('Worker/Dictionary: received { data }', data);
   const MIN_WORD_LENGTH = 3;
+  const MAX_WORD_LENGTH = 10;
   
   const text = String(data).toUpperCase();
-  const dict = text.match(/([A-Z])\w+/g);
+  const dict = text.match(/\b[^\d\W_]+\b/g);
   const res = dict && dict.length ? dict.reduce((acc, current) => {
-    if (current && current.length >= MIN_WORD_LENGTH && !acc.includes(current)) {
+    if (current 
+        && current.length >= MIN_WORD_LENGTH 
+        && current.length <= MAX_WORD_LENGTH 
+        && !acc.includes(current)
+      ) {
       return [...acc, current];
     } else {
       return acc
