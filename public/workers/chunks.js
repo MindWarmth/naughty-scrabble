4f048b8da054c6dec601754d73528b48ce4be9f9
Roman Vabishchevych <r@vabishchevych.com>
2020-11-22 05:48:57 +0200

    chunks calculation

--- public/workers/chunks.js
+++ public/workers/chunks.js
@@ -1,6 +1,7 @@
-onmessage = function({ data: { fieldsData } }) {
+onmessage = function({ data: { fieldsData, foundWords } }) {
+  console.info('Worker/Chunks: received { data: { fieldsData, foundWords } }', fieldsData, foundWords);
   const lenght = fieldsData.length;
-  let chunks = {
+  let chunks = foundWords || {
     data: {},
     list: []
   };
@@ -11,14 +12,14 @@ onmessage = function({ data: { fieldsData } }) {
     let startCol = null;
 
     for (let col = 0; col < lenght; col++) {
-      let { value } = fieldsData[row][col] || {};
+      let { value, checked, horizontal } = fieldsData[row][col] || {};
 
-      if (value) {
+      if (value && !(checked && horizontal)) {
         if (!word) { startCol = col };
         word += value;
 
         // if end of word and unique
-        if (!fieldsData[row][col + 1] && !chunks.data[word]) {
+        if ((!fieldsData[row][col + 1] || fieldsData[row][col + 1].startHorizontal) && !chunks.data[word]) {
           chunks.list.push(word);
           chunks.data[word] = { word, start: [row, startCol], end: [row, col], type: 'horizontal'};
           word = '';
@@ -34,14 +35,14 @@ onmessage = function({ data: { fieldsData } }) {
     let startRow = null;
     
     for (let row = 0; row < lenght; row++) {
-      let { value } = fieldsData[row][col] || {};
+      let { value, checked, vertical } = fieldsData[row][col] || {};
 
-      if (value) {
+      if (value && !(checked && vertical)) {
         if (!word) { startRow = row; }
         word += value;
 
         // if end of word and unique
-        if (!fieldsData[row + 1][col] && !chunks.data[word]) {
+        if ((!fieldsData[row + 1][col] ||  fieldsData[row + 1][col].startVertical) && !chunks.data[word]) {
           chunks.list.push(word);
           chunks.data[word] = { word, start: [startRow, col], end: [row, col], type: 'vertical'};
           word = '';
