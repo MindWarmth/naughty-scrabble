6e940dc1763201587aa50567157eb2c6845f46ca
Roman Vabishchevych <r@vabishchevych.com>
2020-11-22 12:30:40 +0200

    add video

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -10,11 +10,22 @@ const Welcome = () => (
     </Grid>
     <Grid item xs={ 12 } sm={ 6 } md={ 5 }>
       <h1>Welcome<br/>to the Naughty Board!</h1>
-      <h2>Press the button below to create a new game.</h2>
+      <h3>There is a real-time peer-to-peer game.</h3>
+      <h3>Press the button below to create a new game.</h3>
       <Button style={{maxWidth: 280}} fullWidth component={ Link } to="/create" color="primary" variant="contained" size="large">
         Play
       </Button>
       <p>To join an existing game you need a link.</p>
+    </Grid>
+    <Grid container direction="row" justify="center" alignItems="center">
+      <Grid item xs={ 12 } >
+        <h1 style={{textAlign: 'center'}}>How it works</h1>
+        <div className="media">
+          <div className="media__inner">
+            <iframe title="How it works" width="560" height="315" src="https://www.youtube.com/embed/vCc7d20QrBw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+          </div>
+        </div>
+      </Grid>
     </Grid>
   </Grid>
 );
